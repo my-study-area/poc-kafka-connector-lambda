4853ca9b25ba4a336b709288e8274bf6ef24c592
Adriano Avelino <adriano-st777@hotmail.com>
2023-12-16 12:13:07 -0300

    adiciona melhorias no producer

--- run_local/multi-schema-same-topic-avro-producer.py
+++ run_local/multi-schema-same-topic-avro-producer.py
@@ -3,109 +3,96 @@ from confluent_kafka import SerializingProducer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.avro import AvroSerializer
 from confluent_kafka.avro import SerializerError
-import avro.schema
+from avro.schema import parse
 from faker import Faker
 
 fake = Faker()
+TOPIC_NAME = 'example-stream-avro'
+SCHEMA_REGISTRY_URL = 'http://localhost:8081'
+BOOTSTRAP_SERVERS = 'localhost:9092'
+SCHEMA_REGISTRY_CLIENT = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL })
 
 # Define multiple Avro schemas for the values
-value_schema_str_1 = """
-{
-   "namespace": "my.test",
-   "name": "value1",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "language",
-       "type" : "string"
-     },
-     {
-       "name" : "greeting",
-       "type" : "string"
-     }
-   ]
+SCHEMAS = {
+    'greeting': """ {
+        "namespace": "my.test",
+        "name": "value1",
+        "type": "record",
+        "fields" : [
+          { "name" : "language", "type" : "string" },
+          { "name" : "greeting", "type" : "string" }
+        ]
+      }
+    """,
+    'user': """ {
+        "namespace": "my.test",
+        "name": "value2",
+        "type": "record",
+        "fields" : [
+          { "name" : "name", "type" : "string" },
+          { "name" : "age", "type" : "int" }
+        ]
+      }
+    """,
+    'key': """ {
+        "namespace": "my.test",
+        "name": "key",
+        "type": "record",
+        "fields" : [
+          { "name" : "timestamp", "type" : "long" }
+        ]
+    }
+    """
 }
-"""
-
-value_schema_str_2 = """
-{
-   "namespace": "my.test",
-   "name": "value2",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "name",
-       "type" : "string"
-     },
-     {
-       "name" : "age",
-       "type" : "int"
-     }
-   ]
-}
-"""
-
-# Define the Avro schema for the key
-key_schema_str = """
-{
-   "namespace": "my.test",
-   "name": "key",
-   "type": "record",
-   "fields" : [
-     {
-       "name" : "timestamp",
-       "type" : "long"
-     }
-   ]
-}
-"""
-
-# Parse the schema strings into AvroSerializer
-value_schema_1 = avro.schema.parse(value_schema_str_1)
-value_schema_2 = avro.schema.parse(value_schema_str_2)
-key_schema = avro.schema.parse(key_schema_str)
-
-# Create a Schema Registry client
-schema_registry_client = SchemaRegistryClient({
-    'url': 'http://localhost:8081'
-})
 
 # Create AvroSerializers using the parsed schemas and the Schema Registry client
-value_serializer_1 = AvroSerializer(schema_str=value_schema_str_1,
-                                    schema_registry_client=schema_registry_client,
-                                    conf={'subject.name.strategy': topic_record_subject_name_strategy})
-value_serializer_2 = AvroSerializer(schema_str=value_schema_str_2,
-                                    schema_registry_client=schema_registry_client,
-                                    conf={'subject.name.strategy': topic_record_subject_name_strategy})
-key_serializer = AvroSerializer(schema_str=key_schema_str,
-                                schema_registry_client=schema_registry_client)
+SERIALIZER = {
+    'greeting': AvroSerializer(schema_str=SCHEMAS.get('greeting'), schema_registry_client=SCHEMA_REGISTRY_CLIENT, conf={'subject.name.strategy': topic_record_subject_name_strategy}),
+    'user': AvroSerializer(schema_str=SCHEMAS.get('user'), schema_registry_client=SCHEMA_REGISTRY_CLIENT, conf={'subject.name.strategy': topic_record_subject_name_strategy}),
+    'key': AvroSerializer(schema_str=SCHEMAS.get('key'), schema_registry_client=SCHEMA_REGISTRY_CLIENT)
+}
+
+# Configuration dictionary for Kafka producers.
+PRODUCER_CONFIG = {
+    'user': {
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'key.serializer': SERIALIZER.get('key'),
+        'value.serializer': SERIALIZER.get('user')
+    },
+    'greeting': {
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'key.serializer': SERIALIZER.get('key'),
+        'value.serializer': SERIALIZER.get('greeting')
+    }
+}
+
+# Create a SerializingProducer instance with the configuration
+PRODUCER = {
+    'user': SerializingProducer(PRODUCER_CONFIG.get('user')),
+    'greeting': SerializingProducer(PRODUCER_CONFIG.get('greeting'))
+}
 
 # Function to create and send a message with a specific value serializer
-def send_message(value_serializer, value):
+def send_message(producer, value):
     try:
-      # SerializingProducer configuration
-      producer_config = {
-          'bootstrap.servers': 'localhost:9092',
-          'key.serializer': key_serializer,
-          'value.serializer': value_serializer
-      }
-
-      # Create a SerializingProducer instance with the configuration
-      serializing_producer = SerializingProducer(producer_config)
-      key = {"timestamp": int(fake.unix_time())}
-      serializing_producer.produce(topic='example-stream-avro', value=value, key=key)
-      serializing_producer.flush()
-      print(f"Message sent successfully")
+        key = {"timestamp": int(fake.unix_time())}
+        producer.produce(topic=TOPIC_NAME, value=value, key=key)
+        producer.flush()
+        print(f"Message sent successfully")
     except SerializerError as e:
         print(f"Message serialization failed: {str(e)}")
+        raise e
     except Exception as e:
         print(f"Message production failed: {str(e)}")
+        raise e
 
 # Send a message using the first value schema
+greeting_producer = PRODUCER.get('greeting')        
 greeting_message = f"Hello, {fake.country()}!"
-value1 = {"language": "ENGLISH", "greeting": greeting_message}
-send_message(value_serializer_1, value1)
+greeting_value = {"language": "ENGLISH", "greeting": greeting_message}
+send_message(greeting_producer, greeting_value)
 
 # Send a message using the second value schema
-value2 = {"name": fake.name(), "age": fake.pyint(min_value=0, max_value=100)}
-send_message(value_serializer_2, value2)
+user_producer = PRODUCER.get('user')
+user_message = {"name": fake.name(), "age": fake.pyint()}
+send_message(user_producer, user_message)
