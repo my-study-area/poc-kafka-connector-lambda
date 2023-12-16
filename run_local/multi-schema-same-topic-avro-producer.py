c5896e976d6d7da9eb448428638e8d4cd800b9d3
Adriano Avelino <adriano-st777@hotmail.com>
2023-12-16 13:11:45 -0300

    refatora o producer kafka

--- run_local/multi-schema-same-topic-avro-producer.py
+++ run_local/multi-schema-same-topic-avro-producer.py
@@ -1,3 +1,4 @@
+import os
 from confluent_kafka.schema_registry import  topic_record_subject_name_strategy
 from confluent_kafka import SerializingProducer
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -7,12 +8,40 @@ from avro.schema import parse
 from faker import Faker
 
 fake = Faker()
-TOPIC_NAME = 'example-stream-avro'
-SCHEMA_REGISTRY_URL = 'http://localhost:8081'
-BOOTSTRAP_SERVERS = 'localhost:9092'
-SCHEMA_REGISTRY_CLIENT = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL })
 
-# Define multiple Avro schemas for the values
+# Function to create a producer with a specific value serializer
+def create_producer(value_schema_str):
+    schema_registry_client = SchemaRegistryClient({'url': SCHEMA_REGISTRY_URL})
+    key_schema_str = SCHEMAS.get('key')
+
+    key_serializer = AvroSerializer(schema_str=key_schema_str, schema_registry_client=schema_registry_client)
+    value_serializer = AvroSerializer(schema_str=value_schema_str, schema_registry_client=schema_registry_client, conf={'subject.name.strategy': topic_record_subject_name_strategy})
+
+    producer_config = {
+        'bootstrap.servers': BOOTSTRAP_SERVERS,
+        'key.serializer': key_serializer,
+        'value.serializer': value_serializer
+    }
+
+    return SerializingProducer(producer_config)
+
+# Function to create and send a message with a specific value serializer
+def send_message(producer, topic_name, value):
+    try:
+        key = {"timestamp": int(fake.unix_time())}
+        producer.produce(topic=topic_name, value=value, key=key)
+        print(f"Message sent successfully")
+    except SerializerError as e:
+        print(f"Message serialization failed: {str(e)}")
+    except Exception as e:
+        print(f"Message production failed: {str(e)}")
+
+# Configurations
+TOPIC_NAME = os.getenv('TOPIC_NAME', 'example-stream-avro')
+SCHEMA_REGISTRY_URL = os.getenv('SCHEMA_REGISTRY_URL', 'http://localhost:8081')
+BOOTSTRAP_SERVERS = os.getenv('BOOTSTRAP_SERVERS', 'localhost:9092')
+
+# Define Avro schemas for the values
 SCHEMAS = {
     'greeting': """ {
         "namespace": "my.test",
@@ -45,54 +74,18 @@ SCHEMAS = {
     """
 }
 
-# Create AvroSerializers using the parsed schemas and the Schema Registry client
-SERIALIZER = {
-    'greeting': AvroSerializer(schema_str=SCHEMAS.get('greeting'), schema_registry_client=SCHEMA_REGISTRY_CLIENT, conf={'subject.name.strategy': topic_record_subject_name_strategy}),
-    'user': AvroSerializer(schema_str=SCHEMAS.get('user'), schema_registry_client=SCHEMA_REGISTRY_CLIENT, conf={'subject.name.strategy': topic_record_subject_name_strategy}),
-    'key': AvroSerializer(schema_str=SCHEMAS.get('key'), schema_registry_client=SCHEMA_REGISTRY_CLIENT)
-}
-
-# Configuration dictionary for Kafka producers.
-PRODUCER_CONFIG = {
-    'user': {
-        'bootstrap.servers': BOOTSTRAP_SERVERS,
-        'key.serializer': SERIALIZER.get('key'),
-        'value.serializer': SERIALIZER.get('user')
-    },
-    'greeting': {
-        'bootstrap.servers': BOOTSTRAP_SERVERS,
-        'key.serializer': SERIALIZER.get('key'),
-        'value.serializer': SERIALIZER.get('greeting')
-    }
-}
-
-# Create a SerializingProducer instance with the configuration
-PRODUCER = {
-    'user': SerializingProducer(PRODUCER_CONFIG.get('user')),
-    'greeting': SerializingProducer(PRODUCER_CONFIG.get('greeting'))
-}
-
-# Function to create and send a message with a specific value serializer
-def send_message(producer, value):
-    try:
-        key = {"timestamp": int(fake.unix_time())}
-        producer.produce(topic=TOPIC_NAME, value=value, key=key)
-        producer.flush()
-        print(f"Message sent successfully")
-    except SerializerError as e:
-        print(f"Message serialization failed: {str(e)}")
-        raise e
-    except Exception as e:
-        print(f"Message production failed: {str(e)}")
-        raise e
-
 # Send a message using the first value schema
-greeting_producer = PRODUCER.get('greeting')        
+greeting_schema_str = SCHEMAS.get('greeting')
+greeting_producer = create_producer(greeting_schema_str)
 greeting_message = f"Hello, {fake.country()}!"
 greeting_value = {"language": "ENGLISH", "greeting": greeting_message}
-send_message(greeting_producer, greeting_value)
+send_message(greeting_producer, TOPIC_NAME,greeting_value)
 
 # Send a message using the second value schema
-user_producer = PRODUCER.get('user')
+user_schema_str = SCHEMAS.get('user')
+user_producer = create_producer(user_schema_str)
 user_message = {"name": fake.name(), "age": fake.pyint()}
-send_message(user_producer, user_message)
+send_message(user_producer, TOPIC_NAME,user_message)
+
+greeting_producer.flush()
+user_producer.flush()
